1bdf339ecd415871ad669e04fdaeb2cfb2b51012
aryan-494 <aryanmishra49712@gmail.com>
2025-12-25 22:37:59 +0530

    Dashboard page code done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
+/* What is this component?
+-> Dashboard where user will come just after log in 
 
-const Dashboard = () => {
-  return (
-    <div>welcome to dashboard </div>
+What stays constant?
+-> the side bar and nav bar will be same there is no change into it 
+
+What changes?
+-> here i will see change in pages. we'll go to the Only
+ the main content changes based on route (Profile, Courses, Settings). from here 
+
+What data does it need?
+-> i have already navbar so i want side bar and one thing that help to change page globaly(according to user) 
+
+What happens while data loads?
+-> we'll setup one loader 
+ */
+
+import { useSelector } from "react-redux";
+import {Sidebar} from "../components/core/Dashboard/Sidebar"
+import { Outlet } from "react-router-dom";
+
+
+
+function Dashboard(){
+
+
+  const {loading : profileLoading} = useSelector((state)=>state.profile)
+  const {loading : authLoading} = useSelector((state)=>(state.auth))
+
+  if (profileLoading || authLoading) {
+    return(
+      <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
+        <div className="spinner"></div>
+      </div>
+    )
+
+
+  }
+
+  return(
+    <div className="relative flex min-h-[calc(100vh-3.5rem)]">
+    <Sidebar/>
+    <div className="h-[calc(100vh-3.5rem)] flex-1 overflow-auto">
+      <div  className="mx-auto w-11/12 max-w-[1000px] py-10">
+        <Outlet/>
+      </div>
+    </div>
+
+    </div>
+
+
   )
+
+
+
+
+
 }
-
-export default Dashboard
-
