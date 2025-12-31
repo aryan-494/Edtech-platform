4949bf150e525eda3e40929618e71d676d41ba66
aryan-494 <aryanmishra49712@gmail.com>
2025-12-31 18:58:11 +0530

    coursesendpoint done

--- src/components/core/Dashboard/MyCourses.js
+++ src/components/core/Dashboard/MyCourses.js
@@ -1,9 +1,28 @@
-import React from 'react'
+/* MyCourses page shows all courses created by an instructor and lets them add a new course */
 
-const MyCourses = () => {
-  return (
-    <div>MyCourses</div>
-  )
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+/* Flow ->
+Get instructor’s auth token
+Call backend → “give me my courses”
+Store courses in state
+Show them in a table
+Provide “Add Course” button */
+
+
+export default function MyCourses() {
+  const {token} = useSelector((state)=>state.auth)
+  const navigate = useNavigate();
+  
+
+  // to add course in the recent page 
+  const [courses , setCourses] = useState();
+  useEffect(()=>{
+    const fetchCourses=async()=>{
+      const result = await fetc
+    }
+  })
+
 }
-
-export default MyCourses
